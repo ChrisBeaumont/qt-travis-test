ee91f83a86045632ed3ff0b5f0e9de0b2e850279
Chris Beaumont <beaumont@hawaii.edu>
2013-05-15 17:03:10 -0400

    glue depndency loading

---
+++ ci/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+pip install numpy --use-mirrors
+pip install astropy --use-mirrors
+pip install scipy --use-mirrors
+pip install skimage --use-mirrors
+pip install IPython --use-mirrors
+pip install pygments --use-mirrors
+pip install pyzmq --use-mirrors
+pip install mock --use-mirrors
+pip install pyavm --user-mirrors
+pip install matplotlib --use-mirrors
